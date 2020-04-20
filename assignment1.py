5613097f6cef71c9f952402b9848f93b48523570
hmasif456 <64026086+hmasif456@users.noreply.github.com>
2020-04-20 20:25:55 +0500

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter your first number = "))
+num2 = int(input("Enter your 2nd number = "))
+print("Addition of numbers is = " , num1 + num2) 
+print("Subtraction of numbers is = " , num1 - num2)
+print("Multiplication of numbers is = " , num1 * num2)
+print("Division of numbers is = " , num1 / num2)
